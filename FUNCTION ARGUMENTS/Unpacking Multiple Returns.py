a1cee6c5f4995bd9e9c75781e9571511336e398a
Anupriya <asg67@njit.edu>
2020-06-01 21:06:03 -0400

    Create Unpacking Multiple Returns.py

---
+++ FUNCTION ARGUMENTS/Unpacking Multiple Returns.py
@@ -0,0 +1,9 @@
+def scream_and_whisper(text):
+    scream = text.upper()
+    whisper = text.lower()
+    return scream, whisper
+
+the_scream ,the_whisper = scream_and_whisper("Anupriya")
+
+print(the_scream) #ANUPRIYA
+print(the_whisper)#anupriya
